8b6fdbd3bc11f8e59ec547bc7aa7d3a4e06633fd
asp002 <94864876+asp002@users.noreply.github.com>
2024-05-16 08:56:13 +0530

    A

--- task3.py
+++ task3.py
@@ -4,6 +4,9 @@ Code for performing task3
 Selecting the residues in Chain A which are near Na Radially and save them'''
 from pymol import cmd
 
+#Radius variable
+r = 5
+
 #Load the pdb file
 cmd.load(fr"c:\Users\LENOVO\Desktop\temp\gtm-475-CYS-1.pdb")
 
@@ -12,12 +15,12 @@ cmd.select("Na_atoms", "name Na")
 
 #Selecting the residues with 'within' command
 #The number after 'within' is the radius in Angstrom
-cmd.select("radial_residues", "chain A within 10 of Na_atoms")
+cmd.select("radial_residues", f"chain A within {r} of Na_atoms")
 
 #Save as pdb
 #cmd.save(fr"C:\Users\LENOVO\Desktop\iGEM\Mutations\gtlmn-7y6i\gtm-{n}-{mutant}-{fnum}.pdb")
 
-#Printing the residue names
+#Printing the residue names & number of residues
 radial_residues = []
 cmd.iterate("radial_residues", "radial_residues.append((resi,resn))")
 print(len(radial_residues))

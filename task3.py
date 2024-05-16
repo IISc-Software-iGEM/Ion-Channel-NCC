d5cf4c95d7eb806f5d0491542390a7533032a9a1
asp002 <94864876+asp002@users.noreply.github.com>
2024-05-16 08:59:27 +0530

    Added radius variable

--- task3.py
+++ task3.py
@@ -24,7 +24,7 @@ cmd.select("radial_residues", f"chain A within {r} of Na_atoms")
 radial_residues = []
 cmd.iterate("radial_residues", "radial_residues.append((resi,resn))")
 print(len(radial_residues))
-radial_residues = tuple(set(radial_residues))
+radial_residues = tuple(set(radial_residues)) #otherwise each aa is counted 4 times
 print(len(radial_residues))
 print(radial_residues)
 

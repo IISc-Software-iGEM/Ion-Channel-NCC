37a666a397bba196687a472717997a920b654adf
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-06-12 13:29:38 +0530

    initial gradient calculation commit
    The gradient is calculated using np.gradient, this is an initial commit
    with a hunch. Yet not verified.

--- codes/potential_analyse/dx_pot_val.py
+++ codes/potential_analyse/dx_pot_val.py
@@ -1,13 +1,21 @@
 from dx_pot_extract import extract
 from dx_coord import coord_to_int
 
+# Cache for storing file data
+file_cache = {}
+
 def val_potential(cx, cy, cz, filepath):
     """
     This file will extract the potential value at cx, cy, cz.
     The convert it to x,y,z and extract the potential value.
     """
 
-    xmin, ymin, zmin, hx, hy, hz, nx, ny, nz, data = extract(filepath, return_data=True)
+    # Check if data for this file is already in cache
+    if filepath not in file_cache:
+        # If not, read it and store it in cache
+        file_cache[filepath] = extract(filepath, return_data=True)
+
+    xmin, ymin, zmin, hx, hy, hz, nx, ny, nz, data = file_cache[filepath]
     x,y,z = coord_to_int(cx, cy, cz, filepath)
     # Extract the potential data
     # Formula for which line to target 
@@ -18,4 +26,4 @@ def val_potential(cx, cy, cz, filepath):
     # Extract the potential from the data
     potential = data[line_number].split()[position]
 
-    return potential
+    return float(potential)
--- codes/potential_analyse/pot_filemake.py
+++ codes/potential_analyse/pot_filemake.py
@@ -1,14 +1,16 @@
 from dx_pot_extract import extract
 from dx_pot_val import val_potential
 from dx_coord import coord_to_int
+from dx_elec import elec
+
 def filemaker(protein, pqr_file, pot_dx_file):
     """
     This will make our custom potential file.
     We want the coordinate, and the potential at that coordinate. 
     also each line will have x, y, z in integer.
-    INFO x y z cx cy cz potential
+    INFO x y z cx cy cz potential ex ey ez
     The INFO should be pqr file's data
-    ATOM   N  Residue A Resi_num    x y z cx cy cz q r potential
+    ATOM   N  Residue A Resi_num    x y z cx cy cz q r potential ex ey ez
 
     Also some pre data, which tells origin and grid length, etc.
     """
@@ -23,17 +25,17 @@ def filemaker(protein, pqr_file, pot_dx_file):
         p.write("Grid length(cx cy cz): " + str(hx) + " " + str(hy) + " " + str(hz) + "\n")
         p.write("Reference pqr file: " + pqr_file + "\n")
         p.write("Reference dx potential file: " + pot_dx_file + "\n\n")
-        i=0
+        print("Written Header Files.")
+
         for line in pqr_data:
             line = line.split()
             cx, cy, cz, q, r = line[-5], line[-4], line[-3], line[-2], line[-1]
             x,y,z = coord_to_int(cx, cy, cz, pot_dx_file)
             potential = val_potential(cx, cy, cz, pot_dx_file)
-            p.write("{:<10} {:<6} {:<6} {:<6} {:<6} {:<10} {:<10} {:<10} {:<10} {:<10}\n".format(
-                '    '.join(line[:-5]), str(x), str(y), str(z), str(cx), str(cy), str(cz), str(q), str(r), str(potential)))
-            i+=1
-            if i>=10:
-                break
+            ex, ey, ez = elec(x, y, z, pot_dx_file)
+            p.write("{:<10} {:<6} {:<6} {:<6} {:<6} {:<10} {:<10} {:<10} {:<10} {:<10} {:<25} {:<25} {:<25}\n".format(
+                '    '.join(line[:-5]), str(x), str(y), str(z), str(cx), str(cy), str(cz), str(q), str(r), str(potential), str(ex), str(ey), str(ez)))
     return
-print("Starting.")
+print("Starting. . .")
 filemaker("7yg0", "/Volumes/Anirudh/IISc/IGEM/Ion-Channel-NCC/codes/potential_analyse/7yg0.pqr", "/Volumes/Anirudh/IISc/IGEM/Ion-Channel-NCC/codes/potential_analyse/7yg0_pot.dx")
+print("Your file is generated successfully.")
